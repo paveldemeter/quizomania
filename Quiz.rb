cd5e1514618294a9443f09f33b3ddf0877f103bf
Pavel Demeter <paveldemeter@192-168-1-11.tpgi.com.au>
2019-09-03 19:16:39 +1000

    Commiting 2nd.

--- Quiz.rb
+++ Quiz.rb
@@ -1,6 +1,8 @@
 require_relative "pop_questions"
 
-puts "Welcome to my quiz"
+puts "Welcome to Quiz-o-mania"
+puts "Press 1 to continue\n, Press 2 for help\n Press X to Exit"
+
 quiz_maker= Quiz_maker.new
 questions= quiz_maker.make_mathquiz
 questions.each { |x| 
